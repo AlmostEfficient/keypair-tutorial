95913643e12a73c1d22e8f99fbf0ee6170141126
Abdullah Raza <42661870+AlmostEfficient@users.noreply.github.com>
2024-01-05 09:33:23 +1300

    Add Readme, complete loader, add scripts

--- generator.js
+++ generator.js
@@ -5,7 +5,7 @@ import fs from 'fs';
 
 // Generate a new mnemonic (seed phrase)
 const mnemonic = bip39.generateMnemonic();
-console.log("Mnemonic (Seed Phrase):", mnemonic);
+console.log('Mnemonic (Seed Phrase):', mnemonic);
 
 // Convert the mnemonic to a seed
 const seed = await bip39.mnemonicToSeed(mnemonic);
@@ -15,20 +15,24 @@ const keypair = Keypair.fromSeed(seedBytes);
 
 // Extracting the secret key
 const secretKey = keypair.secretKey;
-console.log("Secret Key Uint8Array:", secretKey);
+console.log('Secret Key Uint8Array:', secretKey);
 
 // Converting the private key to a base58 string
-const base58PrivateKey = bs58.encode(keypair.secretKey)
-console.log("base58 PK: " + base58PrivateKey)
+const base58PrivateKey = bs58.encode(keypair.secretKey);
+console.log('base58 PK: ' + base58PrivateKey);
 
 // The secret key is a Uint8Array, you might want to convert it to a more readable format
 // such as a hexadecimal string for display or storage purposes
-const secretKeyHex = Array.from(secretKey).map((byte) => byte.toString(16).padStart(2, '0')).join('');
-console.log("Secret Key Hexadecimal:", secretKeyHex);
+const secretKeyHex = Array.from(secretKey)
+  .map((byte) => byte.toString(16).padStart(2, '0'))
+  .join('');
+console.log('Secret Key Hexadecimal:', secretKeyHex);
 
-// Write all of these values to a .env file
-function appendToEnvFile(key, value) {
-    fs.appendFileSync('.env', `\n${key}=${value}`);
+const filePath = '.env';
+
+if (fileExists(filePath)) {
+  fs.unlinkSync(filePath);
+  console.log(`Old ${filePath} has been deleted.`);
 }
 
 appendToEnvFile('SECRET_PHRASE', mnemonic);
@@ -36,6 +40,20 @@ appendToEnvFile('BYTE_ARRAY', JSON.stringify(Array.from(secretKey)));
 appendToEnvFile('BASE58_STRING', base58PrivateKey);
 appendToEnvFile('HEX_STRING', secretKeyHex);
 
-console.log("Values written to .env file");
-console.log("Public Key:", keypair.publicKey.toBase58());
+console.log('Values written to .env file');
+console.log('Public Key:', keypair.publicKey.toBase58());
 
+// File Helpers
+// Write all of these values to a .env file
+function appendToEnvFile(key, value) {
+  fs.appendFileSync('.env', `\n${key}=${value}`);
+}
+
+function fileExists(filePath) {
+  try {
+    fs.accessSync(filePath);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
